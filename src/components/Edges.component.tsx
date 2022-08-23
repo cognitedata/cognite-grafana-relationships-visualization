736951e8c4d43cb1ef9f36374f526fb62eb2abfe
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-08-23 14:15:33 +0300

    reomved extra field

--- src/components/Edges.component.tsx
+++ src/components/Edges.component.tsx
@@ -14,7 +14,6 @@ export const EdgesEditor: React.FC<any> = ({ onChange, value, item: { defaultVal
     }, [value]),
   };
   const pathValue = (path: string[]) => getDefaultValue(value, path);
-  // console.log(pathValue(['smooth', 'forceDirection']));
   return (
     <div>
       {CustomField('Colors')}
